d2522cf496da53d2017ad1a9a14a7c17fda0641d
Tony <tony.liu0112@gmail.com>
2023-11-30 15:30:19 +0800

    support version 3

--- js/background.js
+++ js/background.js
@@ -18,30 +18,38 @@ chrome.webNavigation.onCreatedNavigationTarget.addListener(function (details) {
 });
 
 function execForMac(details) {
-    chrome.tabs.executeScript(details.tabId, {
-        code: `
-                        window.addEventListener('wheel', function(event) {
-                          console.log(event.deltaX);
-                          if (event.deltaX < 0) {
-                            setTimeout(function() {
-                                window.close();
-                            }, 200);
-                          }
-                        });
-                      `
+    chrome.scripting.executeScript({
+        target: {tabId: details.tabId},
+        function: function () {
+            let i = 0;
+            window.addEventListener('wheel', function (event) {
+                if (event.deltaX < -20) {
+                    i += event.deltaX
+                }
+
+                if (i < -500) {
+                    event.stopPropagation();
+                    event.preventDefault();
+                    window.close();
+                }
+            });
+        }
     });
 }
 
 function execForWindows(details) {
-    chrome.tabs.executeScript(details.tabId, {
-        code: `
-                        document.addEventListener('mousedown', function(event) {
-                            if (event.button === 3) {
-                                setTimeout(function() {
-                                    window.close();
-                                }, 200);
-                            }
-                        });
-                      `
+    chrome.scripting.executeScript({
+        target: {tabId: details.tabId},
+        function: function () {
+            document.addEventListener('mousedown', function (event) {
+                if (event.button === 3) {
+                    event.stopPropagation();
+                    event.preventDefault();
+                    setTimeout(function () {
+                        window.close();
+                    }, 200);
+                }
+            });
+        }
     });
 }
